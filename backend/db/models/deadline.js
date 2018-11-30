8d17e9714e2635b105a1d5ddbcb062986b0e76c2
leehwijin <cysoon96@gmail.com>
2018-12-01 00:39:04 +0900

    regist HomeWork page

--- backend/db/models/deadline.js
+++ backend/db/models/deadline.js
@@ -1,6 +1,10 @@
 const { mongoose, autoIncrement } = require('../mongo')
 
 const deadlineSchema = new mongoose.Schema({
+    hwId: {
+        type: Number,
+        unique: true
+    },
     userId: {
         type: Number,
         required: true
@@ -19,16 +23,15 @@ const deadlineSchema = new mongoose.Schema({
         required: true
     },
     hw_date: {
-        type: Number,
+        type: Date,
         required: true
     }
 })
 
-// doctorId를 Auto Increment 필드로 지정
-// deadlineSchema.plugin(autoIncrement, {
-//     model: 'Deadline',
-//     field: 'userId',
-//     startAt: 1
-// })
+deadlineSchema.plugin(autoIncrement, {
+    model: 'Deadline',
+    field: 'hwId',
+    startAt: 1
+})
 
 module.exports = mongoose.model('Deadline', deadlineSchema)
