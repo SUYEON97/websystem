0c62387b07146aed1021b945b39c5cea9d75d3fb
lsy1004j <lsy1004j@ajou.ac.kr>
2018-12-01 00:49:56 +0900

    라우터 가드
    라우터 가드1

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -4,7 +4,7 @@
         <router-link :to="{name: 'Menu', params: {name: this.name}}">menu</router-link> |
         <router-link :to="{name: 'Information', params: {name: this.name}}">information</router-link> |
         <router-link :to="{name: 'Login'}">log out</router-link>
-        <p>{{greeting}} {{name}}</p>
+        <p>Hello {{name}}</p>
     </div>
 </template>
 <script>
@@ -12,13 +12,7 @@ export default {
     data(){
         return{
             name: this.$route.params.name,
-            greeting: 'who are you'
         }
     },
-    methods:{
-        greeting(){
-            if(name) this.greeting = "Hello" 
-        }
-    }
 }
 </script>
