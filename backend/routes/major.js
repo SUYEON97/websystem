0b34b06a48a7a2b861ef10dc22c50bf7f38ce4c2
leehwijin <cysoon96@gmail.com>
2018-12-04 22:15:05 +0900

    plus calender

--- backend/routes/major.js
+++ backend/routes/major.js
@@ -77,9 +77,12 @@ router.get('/subject', function(req, res, next) {
 
 
 router.post('/register', (req, res) => { 
-    var aaa = new Date(Date.UTC(req.body.deadlineYear,req.body.deadlineMonth-1,req.body.deadlineDate)) - Date.now();
+    var aaa = new Date(req.body.hdate) - Date.now();
+    var bbb = new Date(req.body.hdate);
+    bbb.setHours(9);
   
-    const newDeadline = new deadlineModel({userId:1, hw_name: req.body.hw_name, major_name: req.body.major_name, subject_name:req.body.subject_name, hw_date : new Date(Date.UTC(req.body.deadlineYear,req.body.deadlineMonth-1,req.body.deadlineDate)), timeRemaining: aaa});
+    const newDeadline = new deadlineModel({userId:1, hw_name: req.body.hw_name, major_name: req.body.major_name, subject_name:req.body.subject_name, hw_date : new Date(bbb), timeRemaining: aaa});
+
     newDeadline.save(err => {
         if (err) return res.status(500).send(err);
         return res.end();
--- frontend/src/components/registTodo/registTodo.vue
+++ frontend/src/components/registTodo/registTodo.vue
@@ -6,7 +6,6 @@
         </div>
 
         <div>
-            <!--v-on:submit.prevent="doctorRegister"-->
                   <span>과를 선택해주세요!</span><br>
                   <select v-model="selected" name="items1" v-on:change="filter">
                     <option v-for="majorlist in majorList" :key="majorlist.id" v-if="majorlist.majorNameId==1">{{majorlist.majorName}}</option>
@@ -22,9 +21,9 @@
         <form v-on:submit.prevent="writeHwname">
             <span>과제이름을 적어주세요!</span><br>
                 <input type="text" name="hwName" v-model="hwName"><br>
-            <span>데드라인을 적어주세요!</span><br>
-            <!--<input type="text" name="date" v-model="deadlineDate">-->
-                <input type="text" name="date" v-model="deadlineYear"><input type="text" name="date" v-model="deadlineMonth"><input type="text" name="date" v-model="deadlineDate">
+            <span>데드라인을 선택해주세요!</span><br>
+                <date-picker v-model="date"/>
+                <input type="text" name="date" v-model="date">
                 <input type="submit" value="확인">
         </form>
 
@@ -32,21 +31,21 @@
 </template>
 
 <script>
+     import DatePicker from 'v-cal-input'
+
     export default{
         name: 'app',
         components: {
-
+            DatePicker
         },
         data: function () {
             return{
+                date: null,
                 majorList:[],
                 subjectList:[],
                 selected: '과를 선택해주세요',
                 selected2: '과목을 선택해주세요',
                 hwName:"",
-                deadlineYear:"",
-                deadlineMonth:"",
-                deadlineDate:"",
                 name: this.$route.params.name
             }
         },
@@ -73,14 +72,14 @@
             },
             writeHwname:function () {
                 this.$http.post('http://localhost:8000/regist/register',
-                    {hw_name: this.hwName,subject_name:this.selected2 ,major_name: this.selected, deadlineYear: this.deadlineYear, deadlineMonth:this.deadlineMonth, deadlineDate:this.deadlineDate}).then( (response)=> {
+                    {hw_name: this.hwName,subject_name:this.selected2 ,major_name: this.selected, hdate:this.date}).then( (response)=> {
                     this.hwName="";
-                    this.deadlineDate="";
-                    this.deadlineMonth="";
-                    this.deadlineYear="";
                     this.selected ="";
                     this.selected2="";
-                    console.log(response.data);
+                    //console.log(this.date)
+                    this.date=null;
+
+                    //console.log(response.data);
                 }).catch(function (error) {
                     console.log(error.response);
                 })
